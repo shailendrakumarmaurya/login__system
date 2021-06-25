d23d9e8c4ca790e635a9e30e91a68e3f3d560165
Shailendrakumar Maurya <42598268+9099237351@users.noreply.github.com>
2021-06-25 21:53:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
 
 
-/*
-This file contains database configuration assuming you are running mysql using user "root" and password ""
-*/
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
